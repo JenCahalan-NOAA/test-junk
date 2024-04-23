66676299b6016ce7984ac8dc78fcf2535cd44cf3
JenCahalan-NOAA <jennifer.cahalan@noaa.gov>
2024-04-23 14:45:44 -0700

    testing whether will add to eclipse

---
+++ junk_r_script.R
@@ -0,0 +1,8 @@
+# TODO: Add comment
+# 
+# Author: JenCahalan-NOAA
+###############################################################################
+
+some.dataframe <- cbind(1:100, rnorm(100, 5, 10), rep("A", "B", 100))
+
+save.image("C:\\Users\\jennifer.cahalan\\Work\\JAC Stuff\\junk git test.RData")
